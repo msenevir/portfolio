df5384e937b3187e1606d6e56fe5e1972d0271e9
Mahen Seneviratne <mseneviratne@innovu.com>
2019-06-30 23:18:15 -0400

    fix deploy script

--- posts.ts
+++ posts.ts
@@ -1,6 +1,6 @@
 export default [
   {
-    "filePath": "/Users/mseneviratne/Sandbox/blog/pages/blog/hello-world.mdx",
+    "filePath": "/Users/mseneviratne/Sandbox/portfolio/pages/blog/hello-world.mdx",
     "urlPath": "/blog/hello-world",
     "title": "hello world",
     "publishDate": "2019-06-30T22:34:19.822Z"
